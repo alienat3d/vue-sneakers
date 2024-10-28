5188ed48bc3330db6b5baf1802337d1035a76de7
Al Zaplin <zaplin.dev@gmail.com>
2024-10-28 15:25:46 +0300

    feat(Disable button & remove isAdded flags from goods) Disables "Make an order" button after sending request to server;

--- src/App.vue
+++ src/App.vue
@@ -9,7 +9,11 @@ import DrawerElement from './components/DrawerElement.vue'
 const goods = ref([])
 const cart = ref([])
 
+// 4.0 Сделаем оповещение пользователя, что заказ отправляется, после того, как он нажмёт на кнопку "Оформить заказ". ↓
+const isCreatingOrder = ref(false)
+
 const drawerOpened = ref(false)
+// 3.0 Теперь сделаем так, чтобы при клике на кнопке "Оформить заказ" в корзине товаров, создавался объект с новым заказом в ветвь БД "orders".
 
 // 1.3 Напишем функцию, которая будет считать общую сумму цен товаров в корзине ↓
 // const totalPrice = cart.value.reduce((acc, item) => acc + item.price, 0)
@@ -21,6 +25,14 @@ const totalPrice = computed(() =>
 // 2.1 Создадим функцию, которая будет вычислять налог от суммы цен в корзине
 // [Переход в DrawerElement]
 const vatPrice = computed(() => Math.round((totalPrice.value * 20) / 100))
+
+// 4.6 Добавим функцию cartIsEmpty, которая будет проверять есть ли что-то в корзине
+const cartIsEmpty = computed(() => cart.value.length === 0)
+
+// 4.4 Проверка для кнопки "Оформить заказ"
+const cartButtonDisabled = computed(
+  () => isCreatingOrder.value || cartIsEmpty.value,
+)
 
 const openDrawer = () => (drawerOpened.value = true)
 
@@ -39,6 +51,26 @@ const addToCart = item => {
 const removeFromCart = item => {
   cart.value.splice(cart.value.indexOf(item), 1)
   item.isAdded = false
+}
+
+// 3.1 Создадим для этого специальную функцию createOrder, куда мы поместим содержимое корзины и общую стоимость товаров. А также очищать корзину после отправления данных на сервер. ↓
+// 4.1 Теперь, перед непосредственно отправкой на сервер данных isCreatingOrder ставим в true.
+const createOrder = async () => {
+  try {
+    isCreatingOrder.value = true
+    const { data } = await axios.post(
+      'https://c3357c2bd0a9e3f6.mokky.dev/orders',
+      { goods: cart.value, totalPrice: totalPrice.value },
+    )
+
+    cart.value = []
+
+    return data
+  } catch (error) {
+    console.error(error)
+  } finally {
+    isCreatingOrder.value = false
+  }
 }
 
 const onClickAddPlus = item =>
@@ -126,6 +158,13 @@ onMounted(async () => {
 
 watch(filters, fetchItems)
 
+// 5.0 Повесим отслеживание изменений в корзине. Чтобы при изменении корзины убирались все флажки isAdded с товаров.
+// 5.1 Кстати, у watch также можно повесить 2-ой аргумент "deep: true", который будет следить не только за самим массивом, но и его содержимым. Однако здесь нам это не нужно, т.к. мы заменяем массив с товарами из корзины на пустой массив после отправки. Следовательно он меняется и код запускается. Но если нам нужно отслеживать именно изменения содержимого, то стоит держать эту опцию в уме.
+watch(cart, () => {
+  goods.value = goods.value.map(item => ({ ...item, isAdded: false }))
+  closeDrawer()
+})
+
 provide('cart', {
   cart,
   openDrawer,
@@ -137,10 +176,16 @@ provide('cart', {
 
 <template>
   <!-- 2.0 Теперь нам нужно сделать, чтобы и в панели корзины считалась общая сумма товаров и налог от неё. Поэтому и сюда мы передаём total-price, а также vat для налога. ↑ -->
+  <!-- 3.2 Добавим новую функцию в компонент DrawerElement -->
+  <!-- [Переход в DrawerElement] -->
+  <!-- 4.2 Добавим эту функцию в компонент DrawerElement -->
+  <!-- [Переход в DrawerElement] -->
   <DrawerElement
     v-if="drawerOpened"
     :total-price="totalPrice"
     :vat-price="vatPrice"
+    :button-disabled="cartButtonDisabled"
+    @create-order="createOrder"
   />
   <div class="w-4/5 m-auto bg-white rounded-xl shadow-xl mt-12">
     <!-- 1.0 Теперь необходимо сделать так, чтобы и общая цена товаров, добавленных в корзину, отображалась в хедере. Для этого мы прокинем в HeaderItem пропс, который будет хранить число общей цены — total-price. -->
--- src/components/DrawerElement.vue
+++ src/components/DrawerElement.vue
@@ -1,10 +1,17 @@
 <script setup>
 import DrawerHeader from './DrawerHeader.vue'
 import CartItemList from './CartItemList.vue'
+// 3.3 Здесь добавим emit с функцией отправки корзины на сервер
+const emit = defineEmits(['createOrder'])
+
 // 2.2 Добавим defineProps
+// 4.3 Добавим также isCreatingOrder в пропсы ↓
+// [Переход в App]
 defineProps({
   totalPrice: Number,
   vatPrice: Number,
+  isCreatingOrder: Boolean,
+  buttonDisabled: Boolean,
 })
 </script>
 
@@ -25,10 +32,14 @@ defineProps({
         <span class="flex-1 border-b border-dashed"></span>
         <strong>{{ totalPrice }} руб.</strong>
       </div>
+      <!-- 3.4 Сюда передаём эту функцию и также пропишем, что эта кнопка должна становится активной только, когда у нас есть значение в totalPrice, т.е. хоть один товар в корзине. -->
+      <!-- 4.5 Передаём её кнопке -->
       <button
+        :disabled="buttonDisabled"
         class="mt-6 bg-lime-500 w-full rounded-xl py-3 text-white font-bold transition hover:bg-lime-600 active:bg-lime-700 disabled:bg-gray-400"
+        @click="() => emit('createOrder')"
       >
-        Оформить заказ
+        {{ totalPrice ? 'Оформить заказ' : 'Корзина пуста' }}
       </button>
     </div>
   </div>
